5cb0b9b270bb20282b73d71e604e711b297f4ffa
R_Shahidy <r_shahidy@iranet.net>
2020-09-27 12:22:19 +0330

    Registration form commit

--- app/src/main/java/com/sematec/myapplication/MainActivity.kt
+++ app/src/main/java/com/sematec/myapplication/MainActivity.kt
@@ -2,10 +2,37 @@ package com.sematec.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("ActivityLifeCycle: ", "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("ActivityLifeCycle: ", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("ActivityLifeCycle: ", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("ActivityLifeCycle: ", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("ActivityLifeCycle: ", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("ActivityLifeCycle: ", "onDestroy")
     }
 }
---
+++ app/src/main/java/com/sematec/myapplication/MyTestJavaActivity.java
@@ -0,0 +1,45 @@
+package com.sematec.myapplication;
+
+import android.util.Log;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyTestJavaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_test_java);
+        Log.d("ActivityLifeCycle: ", "onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("ActivityLifeCycle: ", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("ActivityLifeCycle: ", "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("ActivityLifeCycle: ", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("ActivityLifeCycle: ", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("ActivityLifeCycle: ", "onDestroy");
+    }
+}
